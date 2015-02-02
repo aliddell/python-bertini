abfd4040cd310e7ce14f272f77d08e0eb0f3c1f5
Alan Liddell <liddell.10@nd.edu>
2015-02-02 16:41:36 -0500

    from .bertini import *

--- __init__.py
+++ __init__.py
@@ -25,5 +25,5 @@ def __naglib_debug():
         raise RuntimeError('unrecognized value for NAGLIB_DEBUG: {0}'.format(debug_str))
 NAGLIB_DEBUG = __naglib_debug()
 
+from .bertini import *
 from .core import *
-import bertini
