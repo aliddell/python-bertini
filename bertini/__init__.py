08e3ba66e9b47548707c63664b3d69a60a8824e8
Alan Liddell <liddell.10@nd.edu>
2015-02-24 16:48:08 -0500

    misc now a part of core

--- bertini/__init__.py
+++ bertini/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, print_function
 
 from .data import compute_NID
-from .fileutils import fprint,parselines,read_points
+from .fileutils import fprint, parselines, read_points
 from .sysutils import BERTINI, call_bertini
--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import, print_function
 
 from .datatypes import *
+from .misc import *
