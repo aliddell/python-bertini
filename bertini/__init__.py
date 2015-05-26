f55557a43009bf4ce70b6b03902ce78828435a4d
Alan Liddell <liddell.10@nd.edu>
2015-05-26 13:21:51 -0400

    parallel BertiniRun take 3

--- bertini/__init__.py
+++ bertini/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, print_function
 
 from .fileutils import fprint, parselines, read_points
-from .sysutils import BERTINI, BertiniRun
+from .sysutils import BERTINI, MPIRUN, PCOUNT, BertiniRun
